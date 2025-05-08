812e619d516c5b4a98e9ffc14413b3a4a2a61164
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2025-05-09 00:34:37 +0300

    wr_microservice - начальный коммит

---
+++ src/main/java/pro/sky/wr_m/WrMApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.wr_m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/wr_m/WrMApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.wr_m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
